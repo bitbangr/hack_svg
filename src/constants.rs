70d96db6cd7b15c0d903c4d8e088df532f3ee6d8
bitbangr <mgj000@hotmail.com>
2023-02-13 15:09:35 -0800

    added more definitions to constanst file

--- src/constants.rs
+++ src/constants.rs
@@ -7,11 +7,23 @@ const COLOR_BLK: &str = "black";
 const COLOR_WHT: &str = "white";
 
 pub const NORTH: usize = 0;
-pub const EAST: usize = 1;
+pub const  EAST: usize = 1;
 pub const SOUTH: usize = 2;
-pub const WEST: usize = 3;
+pub const  WEST: usize = 3;
 
-pub const TOP: usize = 0;
-pub const RIGHT: usize = 1;
+pub const NW_CORNER: usize = 0;
+pub const NE_CORNER: usize = 1; 
+pub const SE_CORNER: usize = 2;
+pub const SW_CORNER: usize = 3;
+
+pub const    TOP: usize = 0;
+pub const  RIGHT: usize = 1;
 pub const BOTTOM: usize = 2;
-pub const LEFT: usize = 3;
+pub const   LEFT: usize = 3;
+
+pub const  TOP_LEFT: usize = 0 ;
+pub const TOP_RIGHT: usize = 1; 
+pub const BOT_RIGHT: usize = 2;
+pub const  BOT_LEFT: usize = 3;
+
+
