c035d0b9437f9633c87b6f993c9e5f7073770f90
bitbangr <mgj000@hotmail.com>
2023-02-13 14:07:56 -0800

    adding a constants file to be shared among modules

---
+++ src/constants.rs
@@ -0,0 +1,17 @@
+const BOARD_SIZE: i32 = 8;
+const RECT_SIZE: i32 = 50;
+const WIDTH: i32 = 100;
+const HEIGHT: i32 = 100;
+const CELL_SIZE: i32 = 100;
+const COLOR_BLK: &str = "black";
+const COLOR_WHT: &str = "white";
+
+pub const NORTH: usize = 0;
+pub const EAST: usize = 1;
+pub const SOUTH: usize = 2;
+pub const WEST: usize = 3;
+
+pub const TOP: usize = 0;
+pub const RIGHT: usize = 1;
+pub const BOTTOM: usize = 2;
+pub const LEFT: usize = 3;
