0fff6c34188f28a9e03202fb3ee172927f7d88a5
bitbangr <mgj000@hotmail.com>
2023-02-23 03:04:17 -0800

    new tests for 3x3 tile mosaic plus rgb CONST values for rgb

--- src/constants.rs
+++ src/constants.rs
@@ -29,4 +29,6 @@ pub const  BOT_LEFT: usize = 3;
 pub const FLAGGED: usize = 987659; // random value unlikely to be used found in problem domain
 pub const RGB_BLACK: (u8,u8,u8) = (0,0,0);
 pub const RGB_WHITE: (u8,u8,u8) = (255,255,255);
+pub const RGB_RED: (u8,u8,u8) = (255,0,0);
 pub const RGB_GREEN: (u8,u8,u8) = (0,255,0);
+pub const RGB_BLUE: (u8,u8,u8) = (0,0,255);
