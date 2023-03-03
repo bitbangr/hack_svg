ecfb30a4d90aa9e0b3e8e1643432abc96ba4fc9e
bitbangr <mgj000@hotmail.com>
2023-03-03 14:54:17 -0800

    more svg tests

--- src/twenty5_tile_square.rs
+++ src/twenty5_tile_square.rs
@@ -11,7 +11,7 @@ use crate::svg_utils::{test_create_svg, create_svg};
 // a 5x5 square mosaic made of of tiles in various colour configurations.
 // tile  For a twenty five tile square mosiac of one single tile the dimension are 5 row by 5 col
 
-// White mosaic with one single black tile in the center
+// Blue mosaic with one single black tile in the center
 pub fn svg_1() {
     let op_svg_file_name = "./svg_output/fiveXfive/output_1.svg";
     let rows: usize = 5;
@@ -23,11 +23,11 @@ pub fn svg_1() {
 
     //  5x5 array     
     let rgb_vec: Vec<Vec<(RGB)>> = vec![
-        vec![RGB_WHITE, RGB_WHITE, RGB_WHITE, RGB_WHITE, RGB_WHITE],
-        vec![RGB_WHITE, RGB_WHITE, RGB_WHITE, RGB_WHITE, RGB_WHITE],
-        vec![RGB_WHITE, RGB_WHITE, RGB_BLACK, RGB_WHITE, RGB_WHITE],
-        vec![RGB_WHITE, RGB_WHITE, RGB_WHITE, RGB_WHITE, RGB_WHITE],
-        vec![RGB_WHITE, RGB_WHITE, RGB_WHITE, RGB_WHITE, RGB_WHITE],
+        vec![RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],
+        vec![RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],
+        vec![RGB_BLUE, RGB_BLUE, RGB_BLACK, RGB_BLUE, RGB_BLUE],
+        vec![RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],
+        vec![RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],
     ];
 
     let rgb_arr = rgb_vec_to_array(rgb_vec);
@@ -51,3 +51,86 @@ pub fn svg_1() {
     );
 } // svg1
 
+// 6x6 Blue mosaic with a 4 black tiles in the center
+pub fn svg_2() {
+    let op_svg_file_name = "./svg_output/fiveXfive/output_2.svg";
+    let rows: usize = 6;
+    let cols: usize = 6;
+    let tiles_per_pane_height: usize = 6;
+    let tiles_per_pane_width: usize = 6;
+    let svg_width = 600;
+    let svg_height = 600;
+
+    //  6x6 array     
+    let rgb_vec: Vec<Vec<(RGB)>> = vec![
+        vec![RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],
+        vec![RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE,  RGB_BLUE],
+        vec![RGB_BLUE, RGB_BLUE, RGB_BLACK, RGB_BLACK, RGB_BLUE, RGB_BLUE],
+        vec![RGB_BLUE, RGB_BLUE, RGB_BLACK, RGB_BLACK, RGB_BLUE, RGB_BLUE],
+        vec![RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],
+        vec![RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],
+    ];
+
+    let rgb_arr = rgb_vec_to_array(rgb_vec);
+    println!("rgb_arr {:?}", &rgb_arr);
+
+    let col_width: i32 = 100;
+    let row_height: i32 = 100;
+    let data_results = create_pane_test_data(rgb_arr, row_height, col_width);
+
+    println!("svg1 data_results = {:?}", data_results);
+
+    let _ = create_svg(
+        op_svg_file_name,
+        svg_width,
+        svg_height,
+        rows,
+        cols,
+        tiles_per_pane_height,
+        tiles_per_pane_width,
+        data_results,
+    );
+} // svg2
+
+// 8x8 Blue mosaic with a 4x4 black tiles in the center
+pub fn svg_3() {
+    let op_svg_file_name = "./svg_output/fiveXfive/output_3.svg";
+    let rows: usize = 8;
+    let cols: usize = 8;
+    let tiles_per_pane_height: usize = 8;
+    let tiles_per_pane_width: usize = 8;
+    let svg_width = 800;
+    let svg_height = 800;
+
+    //  8x8 array     
+    let rgb_vec: Vec<Vec<(RGB)>> = vec![
+        vec![RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],
+        vec![RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],
+        vec![RGB_BLUE, RGB_BLUE, RGB_RED, RGB_BLACK, RGB_BLACK, RGB_BLACK, RGB_BLUE, RGB_BLUE],
+        vec![RGB_BLUE, RGB_BLUE, RGB_RED, RGB_BLACK, RGB_BLACK, RGB_BLACK, RGB_BLUE, RGB_BLUE],
+        vec![RGB_BLUE, RGB_BLUE, RGB_RED, RGB_BLACK, RGB_BLACK, RGB_WHITE, RGB_BLUE, RGB_BLUE],
+        vec![RGB_BLUE, RGB_BLUE, RGB_BLACK, RGB_BLACK, RGB_BLACK, RGB_BLACK, RGB_BLUE, RGB_BLUE],
+        vec![RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],
+        vec![RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],
+    ];
+
+    let rgb_arr = rgb_vec_to_array(rgb_vec);
+    println!("rgb_arr {:?}", &rgb_arr);
+
+    let col_width: i32 = 100;
+    let row_height: i32 = 100;
+    let data_results = create_pane_test_data(rgb_arr, row_height, col_width);
+
+    println!("svg1 data_results = {:?}", data_results);
+
+    let _ = create_svg(
+        op_svg_file_name,
+        svg_width,
+        svg_height,
+        rows,
+        cols,
+        tiles_per_pane_height,
+        tiles_per_pane_width,
+        data_results,
+    );
+} // svg3
