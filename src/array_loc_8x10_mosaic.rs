d35cfc46e2269a518c23cfbfa14cdfe5e1fd76c7
bitbangr <mgj000@hotmail.com>
2023-03-08 03:38:11 -0800

    utility file

---
+++ src/array_loc_8x10_mosaic.rs
@@ -0,0 +1,23 @@
+fn test(){
+
+let _rgb_vec: Vec<Vec<(RGB)>> = vec![
+        vec![RGB_RED, RGB_BLUE, RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_GREEN, RGB_BLUE, RGB_BLUE, RGB_BLUE],
+        //   [0,0],     [0,1],     [0,2],     [0,3],     [0,4],     [0,5],     [0,6],     [0,7],     [0,8],     [0,9],
+        vec![RGB_BLUE, RGB_BLUE, RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],
+        //   [1,0],     [1,1],     [1,2],     [1,3],     [1,4],     [1,5],     [1,6],     [1,7],     [1,8],     [1,9],
+        vec![RGB_BLUE, RGB_BLUE, RGB_BLACK, RGB_BLACK, RGB_BLUE,  RGB_BLUE,  RGB_BLACK, RGB_BLACK, RGB_BLUE, RGB_GREEN],
+        //   [2,0],     [2,1],     [2,2],     [2,3],     [2,4],     [2,5],     [2,6],     [2,7],     [2,8],     [2,9],
+        vec![RGB_BLUE, RGB_BLUE, RGB_BLACK, RGB_BLACK, RGB_BLUE,  RGB_BLUE,  RGB_BLACK, RGB_BLACK, RGB_BLUE, RGB_BLUE],
+        //   [3,0],     [3,1],     [3,2],     [3,3],     [3,4],     [3,5],     [3,6],     [3,7],     [3,8],     [3,9],
+        vec![RGB_BLUE, RGB_BLUE, RGB_BLACK, RGB_BLACK, RGB_BLUE,  RGB_BLUE,  RGB_BLACK, RGB_BLACK, RGB_BLUE, RGB_BLUE],
+        //   [4,0],     [4,1],     [4,2],     [4,3],     [4,4],     [4,5],     [4,6],     [4,7],     [4,8],     [4,9],
+        vec![RGB_BLUE, RGB_BLUE, RGB_BLACK, RGB_BLACK, RGB_BLUE,  RGB_BLUE,  RGB_BLACK, RGB_BLACK, RGB_BLUE, RGB_BLUE],
+        //   [5,0],     [5,1],     [5,2],     [5,3],     [5,4],     [5,5],     [5,6],     [5,7],     [5,8],     [5,9],
+        vec![RGB_YELLOW, RGB_YELLOW, RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],
+        //   [6,0],     [6,1],     [6,2],     [6,3],     [6,4],     [6,5],     [6,6],     [6,7],     [6,8],     [6,9],
+        vec![RGB_YELLOW, RGB_YELLOW, RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],
+        //   [7,0],     [7,1],     [7,2],     [7,3],     [7,4],     [7,5],     [7,6],     [7,7],     [7,8],     [7,9],
+
+    ];
+
+}
