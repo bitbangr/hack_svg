7cb3e850d4d703d8579034f513c2eda7b91f5e6f
bitbangr <mgj000@hotmail.com>
2023-03-11 22:02:48 -0800

    finished svg1 mosaic test setup

--- src/one_44_tile.rs
+++ src/one_44_tile.rs
@@ -1,7 +1,7 @@
 #![allow(unused)]
 
 use euclid::default::Box2D;
-use crate::constants::{RGB_BLACK,RGB_WHITE,RGB_RED,RGB_GREEN,RGB_BLUE, RGB_YELLOW, RGB_RAW_TITA, RGB_VAN_GREY, RGB_SAP_GREEN, RGB_BRNT_SIENNA, RGB_BRNT_UMBER, RGB_QCRI_VIOLET, RGB_CULN_BLUE, RGB_DIOX_VIOLET, RGB_RAW_UMBER, RGB_RAW_SIENNA};
+use crate::constants::{RGB_BLACK,RGB_WHITE,RGB_RED,RGB_GREEN,RGB_BLUE, RGB_YELLOW, RGB_RAW_TITA, RGB_VAN_GREY, RGB_SAP_GREEN, RGB_BRNT_SIENNA, RGB_BRNT_UMBER, RGB_QCRI_VIOLET, RGB_CULN_BLUE, RGB_DIOX_VIOLET, RGB_RAW_UMBER, RGB_RAW_SIENNA, RGB_PAYNES_GREY, RGB_VIOLT_OXIDE, RGB_YEL_OXIDE};
 use crate::create_tile;
 use crate::mosaic_tile::{RGB, rgb_vec_to_array};
 use crate::pane_build_utils::create_pane_test_data;
@@ -30,23 +30,27 @@ pub fn svg_1() {
         //   [3,0],           [3,1],         [3,2],           [3,3],         [3,4],         [3,5],         [3,6],          [3,7],         [3,8],        [3,9],          [3,10],        [3,11],
         vec![RGB_DIOX_VIOLET, RGB_RAW_UMBER, RGB_BRNT_UMBER,  RGB_VAN_GREY,  RGB_VAN_GREY,  RGB_VAN_GREY,  RGB_RAW_SIENNA, RGB_VAN_GREY, RGB_VAN_GREY, RGB_BRNT_SIENNA, RGB_SAP_GREEN, RGB_CULN_BLUE],
         
+        //   [4,0],          [4,1],          [4,2],          [4,3],         [4,4],           [4,5],            [4,6],        [4,7],          [4,8],         [4,9],           [4,10],          [4,11],
+        vec![RGB_BRNT_UMBER, RGB_BRNT_UMBER, RGB_SAP_GREEN,  RGB_VAN_GREY,  RGB_VAN_GREY,    RGB_VAN_GREY,     RGB_VAN_GREY, RGB_VAN_GREY,    RGB_VAN_GREY, RGB_VAN_GREY,    RGB_PAYNES_GREY, RGB_BRNT_UMBER],
+        //   [5,0],         [5,1],           [5,2],          [5,3],         [5,4],           [5,5],            [5,6],        [5,7],           [5,8],        [5,9],           [5,10],          [5,11],
+        vec![RGB_SAP_GREEN, RGB_SAP_GREEN, RGB_SAP_GREEN,  RGB_SAP_GREEN,  RGB_QCRI_VIOLET,  RGB_BRNT_SIENNA,  RGB_VAN_GREY, RGB_BRNT_SIENNA, RGB_VAN_GREY, RGB_BRNT_SIENNA, RGB_DIOX_VIOLET, RGB_BRNT_SIENNA],
         
-        //   [4,0],     [4,1],     [4,2],     [4,3],     [4,4],     [4,5],     [4,6],     [4,7],     [4,8],     [4,9],   [4,10],  [4,11],
-        vec![RGB_RED, RGB_BLUE, RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_GREEN, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],
-        //   [5,0],     [5,1],     [5,2],     [5,3],     [5,4],     [5,5],     [5,6],     [5,7],     [5,8],     [5,9],   [5,10],  [5,11],
-        vec![RGB_RED, RGB_BLUE, RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_GREEN, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],
-        //   [6,0],     [6,1],     [6,2],     [6,3],     [6,4],     [6,5],     [6,6],     [6,7],     [6,8],     [6,9],   [6,10],  [6,11],
-        vec![RGB_RED, RGB_BLUE, RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_GREEN, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],
-        //   [7,0],     [7,1],     [7,2],     [7,3],     [7,4],     [7,5],     [7,6],     [7,7],     [7,8],     [7,9],   [7,10],  [7,11],
-        vec![RGB_RED, RGB_BLUE, RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_GREEN, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],
-        //   [8,0],     [8,1],     [8,2],     [8,3],     [8,4],     [8,5],     [8,6],     [8,7],     [8,8],     [8,9],   [8,10],  [8,11],
-        vec![RGB_RED, RGB_BLUE, RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_GREEN, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],
-        //   [9,0],     [9,1],     [9,2],     [9,3],     [9,4],     [9,5],     [9,6],     [9,7],     [9,8],     [9,9],   [9,10],  [9,11],
-        vec![RGB_RED, RGB_BLUE, RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_GREEN, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],
-        //   [10,0],    [10,1],   [10,2],    [10,3],    [10,4],    [10,5],    [10,6],    [10,7],     [10,8],   [10,9],  [10,10], [10,11],
-        vec![RGB_RED, RGB_BLUE, RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_GREEN, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],
-        //   [11,0],    [11,1],   [11,2],    [11,3],    [11,4],    [11,5],    [11,6],    [11,7],     [11,8],   [11,9],  [11,10], [11,11],
-        vec![RGB_RED, RGB_BLUE, RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_BLUE,  RGB_GREEN, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE, RGB_BLUE],                
+        //   [6,0],          [6,1],           [6,2],            [6,3],            [6,4],            [6,5],            [6,6],        [6,7],          [6,8],        [6,9],          [6,10],          [6,11],
+        vec![RGB_BRNT_UMBER, RGB_SAP_GREEN,   RGB_DIOX_VIOLET,  RGB_BRNT_UMBER,   RGB_BRNT_SIENNA,  RGB_BRNT_SIENNA,  RGB_VAN_GREY, RGB_VAN_GREY,   RGB_VAN_GREY, RGB_VAN_GREY,   RGB_PAYNES_GREY, RGB_BRNT_SIENNA],
+        //   [7,0],          [7,1],           [7,2],            [7,3],            [7,4],            [7,5],            [7,6],        [7,7],          [7,8],        [7,9],          [7,10],          [7,11],
+        vec![RGB_BRNT_UMBER, RGB_DIOX_VIOLET, RGB_DIOX_VIOLET,  RGB_QCRI_VIOLET,  RGB_VAN_GREY,     RGB_BRNT_SIENNA,  RGB_VAN_GREY, RGB_RAW_SIENNA, RGB_VAN_GREY, RGB_RAW_SIENNA, RGB_BRNT_UMBER,  RGB_BRNT_UMBER],
+
+
+        //   [8,0],           [8,1],           [8,2],            [8,3],           [8,4],           [8,5],           [8,6],           [8,7],        [8,8],        [8,9],         [8,10],          [8,11],
+        vec![RGB_PAYNES_GREY, RGB_PAYNES_GREY, RGB_PAYNES_GREY,  RGB_BRNT_UMBER,  RGB_BRNT_UMBER,  RGB_BRNT_UMBER,  RGB_BRNT_SIENNA, RGB_VAN_GREY, RGB_VAN_GREY, RGB_VAN_GREY,  RGB_BRNT_SIENNA, RGB_BRNT_UMBER],
+        //   [9,0],           [9,1],           [9,2],            [9,3],           [9,4],           [9,5],           [9,6],           [9,7],        [9,8],        [9,9],         [9,10],          [9,11],
+        vec![RGB_PAYNES_GREY, RGB_DIOX_VIOLET, RGB_PAYNES_GREY,  RGB_SAP_GREEN,   RGB_BRNT_UMBER,  RGB_BRNT_UMBER,  RGB_VIOLT_OXIDE, RGB_VAN_GREY, RGB_VAN_GREY, RGB_YEL_OXIDE, RGB_VAN_GREY,    RGB_QCRI_VIOLET],
+
+
+        //   [10,0],          [10,1],          [10,2],           [10,3],           [10,4],         [10,5],          [10,6],          [10,7],       [10,8],       [10,9],         [10,10],          [10,11],
+        vec![RGB_PAYNES_GREY, RGB_PAYNES_GREY, RGB_PAYNES_GREY,  RGB_PAYNES_GREY,  RGB_RAW_UMBER,  RGB_BRNT_UMBER,  RGB_BRNT_SIENNA, RGB_VAN_GREY, RGB_VAN_GREY, RGB_VAN_GREY,    RGB_VAN_GREY,    RGB_BRNT_UMBER],
+        //   [11,0],          [11,1],          [11,2],           [11,3],           [11,4],         [11,5],          [11,6],          [11,7],       [11,8],       [11,9],          [11,10],         [11,11],
+        vec![RGB_PAYNES_GREY, RGB_PAYNES_GREY, RGB_PAYNES_GREY,  RGB_PAYNES_GREY,  RGB_SAP_GREEN,  RGB_BRNT_UMBER,  RGB_QCRI_VIOLET, RGB_VAN_GREY, RGB_VAN_GREY, RGB_QCRI_VIOLET, RGB_PAYNES_GREY, RGB_DIOX_VIOLET],                
     ];
 
     let rgb_arr = rgb_vec_to_array(rgb_vec);
