c1e1dcf5d97fd2d80b555105bf31bbe476551792
bitbangr <mgj000@hotmail.com>
2023-03-14 13:47:41 -0700

    added function to read rgb array input from a JSON file

--- src/one_44_tile.rs
+++ src/one_44_tile.rs
@@ -73,3 +73,101 @@ pub fn svg_1() {
         data_results,
     );
 } // svg1
+
+
+pub fn svg_frank() {
+    let op_svg_file_name = "./svg_output/twelveXtwelve/frank.svg";
+    let rows: usize = 40;
+    let cols: usize = 40;
+    let tiles_per_pane_height: usize = 40;
+    let tiles_per_pane_width: usize = 40;
+    let svg_width = 4000;
+    let svg_height = 4000 ;
+
+    let rgb_vec_result = read_config("./franky.json");
+    let rgb_vec = match rgb_vec_result {
+        Ok(rgb_vec) => rgb_vec,
+        Err(e) => {
+            println!("Error reading config: {:?}", e);
+            panic!();
+        }
+    };
+    
+    let rgb_arr = rgb_vec_to_array(rgb_vec);
+
+    let col_width: i32 = 100;
+    let row_height: i32 = 100;
+    let data_results = create_pane_test_data(rgb_arr, row_height, col_width);
+
+    // println!("svg1 data_results = {:?}", data_results);
+
+    let _ = create_svg(
+        op_svg_file_name,
+        svg_width,
+        svg_height,
+        rows,
+        cols,
+        tiles_per_pane_height,
+        tiles_per_pane_width,
+        data_results,
+    );
+} // svg_frank
+
+pub fn svg_frank_tr12() {
+    let op_svg_file_name = "./svg_output/twelveXtwelve/frank_tr12.svg";
+    let rows: usize = 12;
+    let cols: usize = 12;
+    let tiles_per_pane_height: usize = 12;
+    let tiles_per_pane_width: usize = 12;
+    let svg_width = 1200;
+    let svg_height = 1200 ;
+
+    let rgb_vec_result = read_config("./frank_tr12.json");
+    let rgb_vec = match rgb_vec_result {
+        Ok(rgb_vec) => rgb_vec,
+        Err(e) => {
+            println!("Error reading config: {:?}", e);
+            panic!();
+        }
+    };
+    
+    let rgb_arr = rgb_vec_to_array(rgb_vec);
+
+    let col_width: i32 = 100;
+    let row_height: i32 = 100;
+    let data_results = create_pane_test_data(rgb_arr, row_height, col_width);
+
+    // println!("svg1 data_results = {:?}", data_results);
+
+    let _ = create_svg(
+        op_svg_file_name,
+        svg_width,
+        svg_height,
+        rows,
+        cols,
+        tiles_per_pane_height,
+        tiles_per_pane_width,
+        data_results,
+    );
+} // svg_frank
+
+
+use serde::{Deserialize, Serialize};
+use std::error::Error;
+use std::fs::File;
+use std::io::Read;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Config {
+    tiles: Vec<Vec<RGB>>,
+}
+
+fn read_config(file_name: &str) -> Result<Vec<Vec<RGB>>, Box<dyn Error>> {
+    let mut file = File::open(file_name)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let config: Config = serde_json::from_str(&contents)?;
+    Ok(config.tiles)
+}
+
